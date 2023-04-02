59663c5c123f7c6416d8fba83c6b66d4e4ec53a8
Karan Rajbhar <kran@outlook.com>
2023-04-02 20:21:35 +0530

    cat completed

--- 1_unix_utilities/cat/cat.go
+++ 1_unix_utilities/cat/cat.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	args := os.Args[1:]
+	// fmt.Println("Command-line arguments:", args[0])
+
+	if len(args) > 0 {
+
+		// Open the file for reading
+		file, err := os.Open(args[0])
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer file.Close()
+
+		// Create a scanner to read the file line by line
+		scanner := bufio.NewScanner(file)
+		for scanner.Scan() {
+			// Print each line of the file
+			fmt.Println(scanner.Text())
+		}
+
+		// Check for any errors during scanning
+		if err := scanner.Err(); err != nil {
+			fmt.Println(err)
+			return
+		}
+	} else {
+		fmt.Println("File Not provided")
+	}
 }
