c9296e6bbe34ace5272aab70bf1525e806656e7e
Karan Rajbhar <kran@outlook.com>
2023-09-05 21:35:49 +0530

    started writing 4th script

---
+++ 4_HTTPal/httpal.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func main() {
+	// Make the HTTP GET request
+	args := os.Args
+
+	if len(args) < 2 {
+		fmt.Println("Error: Please provide URL.")
+		os.Exit(1)
+	}
+
+	URL := args[1]
+	resp, err := http.Get(URL)
+	if err != nil {
+		fmt.Println("Error making HTTP request:", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	// Read the response body
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error reading response body:", err)
+		return
+	}
+
+	err = os.WriteFile("response.txt", body, 0644)
+	if err != nil {
+		fmt.Println("Error saving response to file:", err)
+		return
+	}
+
+}
